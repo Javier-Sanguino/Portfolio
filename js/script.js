4a20a2bf1cd7ed8f340a81c9efe6c2ffc6c08405
Javier Sanguino E <fjse0505@gmail.com>
2026-01-07 11:07:39 -0500

    Commit: Animaciones

--- js/script.js
+++ js/script.js
@@ -1,164 +1,179 @@
-/**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
- */
-
 (function ($) {
-	'use strict';
+  "use strict";
 
+  /* ========================================================================= */
+  /*	Page Preloader
 	/* ========================================================================= */
-	/*	Page Preloader
+
+  $(window).on("load", function () {
+    // preloader
+    $(".preloader").fadeOut(700);
+
+    // Portfolio Filtering
+    var containerEl = document.querySelector(".filtr-container");
+    var filterizd;
+    if (containerEl) {
+      filterizd = $(".filtr-container").filterizr({});
+    }
+    //Active changer
+    $(".portfolio-filter button").on("click", function () {
+      $(".portfolio-filter button").removeClass("active");
+      $(this).addClass("active");
+    });
+  });
+
+  /* ========================================================================= */
+  /*	Post image slider
 	/* ========================================================================= */
+  $("#post-thumb, #gallery-post").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+  $("#features").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
 
-	$(window).on('load', function () {
-		// preloader
-		$('.preloader').fadeOut(700);
-
-		// Portfolio Filtering
-		var containerEl = document.querySelector('.filtr-container');
-		var filterizd;
-		if (containerEl) {
-			filterizd = $('.filtr-container').filterizr({});
-		}
-		//Active changer
-		$('.portfolio-filter button').on('click', function () {
-			$('.portfolio-filter button').removeClass('active');
-			$(this).addClass('active');
-		});
-	});
-
-	/* ========================================================================= */
-	/*	Post image slider
-	/* ========================================================================= */
-	$('#post-thumb, #gallery-post').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-
-	});
-	$('#features').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-	});
-
-
-	/* ========================================================================= */
-	/*	Magnific popup
+  /* ========================================================================= */
+  /*	Magnific popup
 	/* =========================================================================  */
-	$('.image-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: false,
-		fixedBgPos: true
-	});
+  $(".image-popup").magnificPopup({
+    type: "image",
+    removalDelay: 160, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        // just a hack that adds mfp-anim class to markup
+        this.st.image.markup = this.st.image.markup.replace(
+          "mfp-figure",
+          "mfp-figure mfp-with-anim"
+        );
+        this.st.mainClass = this.st.el.attr("data-effect");
+      },
+    },
+    closeOnContentClick: true,
+    midClick: true,
+    fixedContentPos: false,
+    fixedBgPos: true,
+  });
 
+  // counterUp
+  function counter() {
+    var oTop;
+    if ($(".jsCounter").length !== 0) {
+      oTop = $(".jsCounter").offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $(".jsCounter").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+          {
+            duration: 500,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
+          }
+        );
+      });
+    }
+  }
 
-	// counterUp
-	function counter() {
-		var oTop;
-		if ($('.jsCounter').length !== 0) {
-			oTop = $('.jsCounter').offset().top - window.innerHeight;
-		}
-		if ($(window).scrollTop() > oTop) {
-			$('.jsCounter').each(function () {
-				var $this = $(this),
-					countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				}, {
-					duration: 500,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						$this.text(this.countNum);
-					}
-				});
-			});
-		}
-	}
+  $(window).on("scroll", function () {
+    counter();
+  });
 
-	$(window).on('scroll', function () {
-		counter();
-	});
+  //   magnific popup video
+  $(".popup-video").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-zoom-in",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: true,
+  });
 
-
-
-	//   magnific popup video
-	$('.popup-video').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-zoom-in',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: true
-	});
-
-	/* ========================================================================= */
-	/*	Testimonial Carousel
+  /* ========================================================================= */
+  /*	Testimonial Carousel
 	/* =========================================================================  */
-	//Init the carousel
-	$('#testimonials').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-	});
+  //Init the carousel
+  $("#testimonials").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
 
-
+  /* ========================================================================= */
+  /*	Smooth Scroll
 	/* ========================================================================= */
-	/*	Smooth Scroll
-	/* ========================================================================= */
-	$('a.nav-link, .smooth-scroll').click(function (e) {
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				e.preventDefault();
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 1000, function () {
-					var $target = $(target);
-					$target.focus();
-					if ($target.is(':focus')) {
-						return false;
-					} else {
-						$target.attr('tabindex', '-1');
-						$target.focus();
-					}
-				});
-			}
-		}
-	});
-
-
+  $("a.nav-link, .smooth-scroll").click(function (e) {
+    if (
+      location.pathname.replace(/^\//, "") ===
+        this.pathname.replace(/^\//, "") &&
+      location.hostname === this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        e.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr("tabindex", "-1");
+              $target.focus();
+            }
+          }
+        );
+      }
+    }
+  });
 })(jQuery);
 // End Jquery Function
-
 
 /* ========================================================================= */
 /*	Animated section
 /* ========================================================================= */
 
 var wow = new WOW({
-	offset: 100, // distance to the element when triggering the animation (default is 0)
-	mobile: false // trigger animations on mobile devices (default is true)
+  offset: 100, // distance to the element when triggering the animation (default is 0)
+  mobile: false, // trigger animations on mobile devices (default is true)
 });
 wow.init();
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+reveals.forEach((el) => observer.observe(el));
